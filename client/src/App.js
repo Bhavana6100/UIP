c57c7b19314db49fd8a6ce115551196e0ba25903
Bhavana6100 <bhavana.g6100@gmail.com>
2023-06-23 02:45:58 -0400

    All files are added to the project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Welcome to Hello world !!
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.dbURL)
 app.use(express.json());
 
 app.use(express.static(__dirname + "/public"));
-app.get('/', (req,res)=>res.sendFile(path.join(__dirname, '/Post/public','index.html')));
+app.get('/', (req,res)=>res.sendFile(path.join(__dirname, '/client/public','index.html')));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");  
@@ -26,6 +26,6 @@ app.use(function(req, res, next) {
 app.use('/user', userRoutes);
 app.use('/posts', postRoutes)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}!!!`))
